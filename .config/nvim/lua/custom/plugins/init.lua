047149ef152562e6bbc6562d0a5740209fdf64a6
Rishikesh Sadanandan <rishikesh.sadanandan@gmail.com>
2024-11-29 22:02:58 -0500

    feat: add keymap to write & commit w git msg

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -7,7 +7,20 @@ return {
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
   -- Git related plugins
-  'tpope/vim-fugitive',
+  {
+    'tpope/vim-fugitive',
+    config = function()
+      -- Define the function
+      local function git_write_and_commit()
+        vim.cmd 'Gwrite'
+        vim.cmd 'Git commit'
+        vim.cmd 'startinsert'
+      end
+
+      -- Map the function to the keybinding
+      vim.keymap.set('n', '<leader>gh', git_write_and_commit, { desc = 'Git write and commit' })
+    end,
+  },
   'tpope/vim-rhubarb',
 
   -- Nerd Font Icons
